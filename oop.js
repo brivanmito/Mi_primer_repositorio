2b21e6d6c37efe4550f97ce28a81d90bcba8347d
brivanmito <bryanmt496@gmail.com>
2025-01-19 01:02:47 -0500

    first commit

---
+++ oop.js
@@ -0,0 +1,17 @@
+class Pelicula {
+
+    constructor(nombre, id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    reproducir() {
+       
+    }
+
+}
+
+
+const peliculaUno = new Pelicula('Harry', 1);
+const peliculaDos = new Pelicula('Araña', 2);
+console.log(peliculaUno);
